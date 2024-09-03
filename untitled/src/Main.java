d904c577049bebb3b49519f05548a93112cfa89b
Coward_Lizzards <searinart@gmail.com>
2024-09-03 15:00:12 -0300

    classe privada

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,7 +4,22 @@ public class Main {
     public static void main(String[] args){
 
     Scanner sc = new Scanner(System.in);
-    Pessoa p = new Pessoa
+    Scanner sc2 = new Scanner(System.in);
+    Pessoa p = new Pessoa();
 
+
+    System.out.println("Seu Nome: ");
+    p.setNome(sc2.nextLine());
+
+    System.out.println("Sua Idade: ");
+    p.setIdade(sc.nextInt());
+
+    System.out.println("Seu Salario: ");
+    p.setSalario(sc.nextDouble());
+
+
+    System.out.println("Nome: "+p.getNome());
+    System.out.println("Idade: "+p.getIdade());
+    System.out.println("Salario: "+p.getSalario());
     }
 }
---
+++ untitled/src/Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa {
+    private String nome;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    private int idade;
+
+    public void setIdade(int idade){
+        this.idade=idade;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+
+
+    private double salario;
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario(){
+        return salario;
+
+
+    }
+}
